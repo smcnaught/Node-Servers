198b34ce6d1f64260216f2562f21908839da41ff
smcnaught <stephaniemcnaught@gmail.com>
2017-09-09 10:40:43 -0600

    Adding urlParsing js file

---
+++ urlParsing.js
@@ -0,0 +1,41 @@
+// url package will allow us to parse parts of the request url.
+var url = require('url');
+
+// require http
+var http = require('http');
+
+// This is the port we are going to listen on.
+var PORT = 8080;
+
+// Function tthat runs when our localhost:PORT  is hit with http traffic or a web browser. 
+function requestHandler(request, response) {
+    var urlParts = url.parse(request.url);
+    // response.end("url: " + JSON.stringify(urlParts));
+    switch (urlParts.pathname) {
+        case "/":
+            response.end("You're in trouble")
+            break;
+        case "/portfolio":
+            response.end("This is my portfolio")
+            break;
+        case "/edit":
+            console.log("display edit");
+            response.end("You're going to edit, eh?");
+            break;
+        case "/fun":
+            response.end("You just won a new car!");
+            break;
+        default:
+            console.log("Might not have worked");
+            break;
+    }
+};
+
+// Create a server that functions with requestHandler as the main function.
+var server = http.createServer(requestHandler);
+
+// Directs the server to listen for http requests on PORT(8081).
+// Also prints to the node console what port we are listening on. 
+server.listen(PORT, function () {
+    console.log("The server is listening on localhost:%s", PORT);
+});
